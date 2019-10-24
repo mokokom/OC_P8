d4ee8e587412ccc04d1a369c6b17357cc156ea66
mokokom <mokokebeloke@gmail.com>
2019-10-24 16:57:28 +0200

    fix the btn issue (all, active and completed that didn't worked) adding anonymous functionaround Todo

--- js/app.js
+++ js/app.js
@@ -10,19 +10,24 @@ import View from "./view.js";
  *
  * @param {string} name The name of your new to do list.
  */
-export default class Todo {
-	constructor(name) {
-		this.storage = new Store(name);
-		this.model = new Model(this.storage);
-		this.template = new Template();
-		this.view = new View(this.template);
-		this.controller = new Controller(this.model, this.view);
+(function() {
+	class Todo {
+		constructor(name) {
+			this.storage = new Store(name);
+			this.model = new Model(this.storage);
+			this.template = new Template();
+			this.view = new View(this.template);
+			this.controller = new Controller(this.model, this.view);
+		}
 	}
-}
 
-var todo = new Todo("todos-vanillajs");
+	var todo = new Todo("todos-vanillajs");
 
-function setView() {
-	todo.controller.setView(document.location.hash); // * location.hash = Return the anchor part of a URL
-}
-setView();
+	function setView() {
+		todo.controller.setView(document.location.hash); // * location.hash = Return the anchor part of a URL
+	}
+	setView();
+
+	$on(window, "load", setView);
+	$on(window, "hashchange", setView);
+})();
