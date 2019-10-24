7566273ae25aadf8fa6e7398b2b614360a458daa
mokokom <mokokebeloke@gmail.com>
2019-10-24 16:24:28 +0200

    app.js change before changing branch

--- js/app.js
+++ js/app.js
@@ -1,30 +1,30 @@
 /*global app, $on */
-/* (function() {
+(function() {
 	"use strict";
-})();
- */
-/**
- * Sets up a brand new Todo list.
- *
- * @param {string} name The name of your new to do list.
- */
-class Todo {
-	constructor(name) {
-		this.storage = new app.Store(name);
-		this.model = new app.Model(this.storage);
-		this.template = new app.Template();
-		this.view = new app.View(this.template);
-		this.controller = new app.Controller(this.model, this.view);
+
+	/**
+	 * Sets up a brand new Todo list.
+	 *
+	 * @param {string} name The name of your new to do list.
+	 */
+	class Todo {
+		constructor(name) {
+			this.storage = new app.Store(name);
+			this.model = new app.Model(this.storage);
+			this.template = new app.Template();
+			this.view = new app.View(this.template);
+			this.controller = new app.Controller(this.model, this.view);
+		}
 	}
-}
 
-var todo = new Todo("todos-vanillajs");
+	var todo = new Todo("todos-vanillajs");
 
-function setView() {
-	todo.controller.setView(document.location.hash); // * location.hash = Return the anchor part of a URL
-}
+	function setView() {
+		todo.controller.setView(document.location.hash); // * location.hash = Return the anchor part of a URL
+	}
 
-setView();
+	setView();
 
-/* $on(window, "load", setView);
-$on(window, "hashchange", setView); */
+	$on(window, "load", setView);
+	$on(window, "hashchange", setView);
+})();
